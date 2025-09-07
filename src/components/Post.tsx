64546fed8d80e4c4e1d6281cdae12ba798463244
Dan <danhemed123@gmail.com>
2025-09-07 15:35:51 +0300

    i create a json with 6 posts and i import the json to the app and i show the post in the web by map

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,33 +1,31 @@
 import './Post.css'
-type post = {
+
+type postType = {
     img_url: string,
     description: string,
     likes: number,
     name_user: string,
     date: string
+};
+
+type postsType = {
+    post: postType
 }
 
-
-export default function Post() {
-    const firstPost: post = {
-        img_url: "/images/dog-image.jpeg",
-        description: "cute dog",
-        likes: 0,
-        name_user: "dan",
-        date: `07/09/2025 11:04`
-    }
-
+export default function Post({post}: postsType) {
     return (
         <article className="post">
-            <img src={firstPost.img_url} alt={firstPost.description} />
+            <div className="block-img">
+            <img src={post.img_url} alt={post.description} />
+            </div>
             <div className="under-post">
                 <div className='des-and-likes'>
-                    <p>{firstPost.description}</p>
-                    <h4>{firstPost.likes}</h4>
+                    <p>{post.description}</p>
+                    <h4>{post.likes}</h4>
                 </div>
                 <div className='name-and-date'>
-                    <h3>{firstPost.name_user}</h3>
-                    <h6>{firstPost.date}</h6>
+                    <h3>{post.name_user}</h3>
+                    <h6>{post.date}</h6>
                 </div>
             </div>
         </article>
