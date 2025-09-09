aa80cf25428fd81f301effa6ce646bd8efea81c0
Dan <danhemed123@gmail.com>
2025-09-09 11:44:07 +0300

    I wasn't able to do it completely but I created a path for each page and it always displays the dynamic page

---
+++ client/src/pages/DynamicPostPage.tsx
@@ -0,0 +1,9 @@
+import '../css/Post.css';
+
+export default function DynamicPostPage() {
+    return (
+        <article className="dynamic-post-page">
+            העמוד הדנמי שמכיל את הפוסט המובקש
+        </article>
+    )
+}
